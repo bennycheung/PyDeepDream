4cd60fe0b0c2855d3b7386aaf4ab97bcc37b09e3
btscheung <btscheung@gmail.com>
2017-03-05 14:18:02 -0500

    add testing run.sh

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+python deepdream.py -d 1 -t 1 -o 6 -i sky_small.jpg
+python deepdream.py -d 2 -t 1 -o 6 -i sky_small.jpg
+python deepdream.py -d 3 -t 1 -o 6 -i sky_small.jpg
+python deepdream.py -d 4 -t 1 -o 6 -i sky_small.jpg
+python deepdream.py -d 5 -t 1 -o 6 -i sky_small.jpg
+python deepdream.py -d 6 -t 1 -o 6 -i sky_small.jpg
+python deepdream.py -d 7 -t 1 -o 6 -i sky_small.jpg
+python deepdream.py -d 8 -t 1 -o 6 -i sky_small.jpg
+python deepdream.py -d 9 -t 1 -o 6 -i sky_small.jpg
